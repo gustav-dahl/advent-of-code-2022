589486ec425619ace076e6c33f7c195f752b70d1
Gustav Gahm <gustav.gahm@sogeti.se>
2022-12-02 08:40:44 +0100

    02

--- 02/Drivers.fs
+++ 02/Drivers.fs
@@ -1,29 +1,104 @@
 module AdventOfCode.Drivers
 
-let split (separators: string) (x: string) = x.Split(separators)
+open AdventOfCode.Utilities
 
-let parseInput (input: string[]) =
+type Play =
+    | Rock
+    | Paper
+    | Scissors
+
+
+let parsePlay x =
+    match x with
+    | "A" -> Rock
+    | "B" -> Paper
+    | "C" -> Scissors
+    | "X" -> Rock
+    | "Y" -> Paper
+    | "Z" -> Scissors
+
+type Result =
+    | Win
+    | Loss
+    | Draw
+
+let parseResult x =
+    match x with
+    | "X" -> Loss
+    | "Y" -> Draw
+    | "Z" -> Win
+
+
+let calculatePlay round =
+    match round with
+    | (x, Draw) -> (x, x)
+    | (Rock, Win) -> (Rock, Paper)
+    | (Paper, Win) -> (Paper, Scissors)
+    | (Scissors, Win) -> (Scissors, Rock)
+    | (Rock, Loss) -> (Rock, Scissors)
+    | (Paper, Loss) -> (Paper, Rock)
+    | (Scissors, Loss) -> (Scissors, Paper)
+
+
+let isWin (x, y) =
+    match (y, x) with
+    // Rock defeats Scissors
+    | (Rock, Scissors) -> Win
+    // Paper defeats Rock.
+    | (Paper, Rock) -> Win
+    // Scissors defeats Paper
+    | (Scissors, Paper) -> Win
+    // If both players choose the same shape, the round instead ends in a draw.
+    | (x, y) -> if x = y then Draw else Loss
+
+let playScore (_, x) =
+    match x with
+    | Rock -> 1
+    | Paper -> 2
+    | Scissors -> 3
+
+let outcomeScore x =
+    match x with
+    | Win -> 6
+    | Draw -> 3
+    | Loss -> 0
+
+let singleRoundScore round =
+    (isWin round |> outcomeScore) + (playScore round)
+
+
+// ----------------------------------------------------------------
+// Part 1
+
+let parseInput1 (input: string[]) =
     input
-    |> String.concat "|"
-    |> split "||"
-    |> Seq.map (split "|")
-    |> Seq.map (fun x -> x |> Seq.map int)
+    |> Array.map (split " ")
+    |> Array.map (fun row -> (parsePlay row[0], parsePlay row[1]))
+
+let expected1 = 15
 
 let part1 (input: string[]) =
-    let elfs = input |> parseInput
-    let calories = elfs |> Seq.map Seq.sum
-    let maxCalories = calories |> Seq.max
+    let score = input |> parseInput1 |> Array.map singleRoundScore |> Seq.sum
 
-    maxCalories
+    score
+
+// ----------------------------------------------------------------
+// Part 2
+
+let parseInput2 (input: string[]) =
+    input
+    |> Array.map (split " ")
+    |> Array.map (fun row -> (parsePlay row[0], parseResult row[1]))
+
+let expected2 = 12
 
 let part2 input =
-    let elfs = input |> parseInput
-    let calories = elfs |> Seq.map Seq.sum
 
-    let maxCalories =
-        calories
-        |> Seq.sortDescending
-        |> Seq.take 3
+    let score =
+        input
+        |> parseInput2
+        |> Array.map calculatePlay
+        |> Array.map singleRoundScore
         |> Seq.sum
 
-    maxCalories
+    score 
--- 02/Program.fs
+++ 02/Program.fs
@@ -1,30 +1,36 @@
 // https://mallibone.com/post/advent-of-f-code
 
+open System
 open AdventOfCode.Input
 open AdventOfCode.Drivers
 open AdventOfCode.Outout
 
-let expected1 = 24000 
-let expected2 = 45000
+Console.Clear()
+
+Console.ForegroundColor <- ConsoleColor.Gray
+printfn "------- Part 1 -------"
 
 // --------------------------------------------
 // Test - Part 1
 let testValue1 = loadTestInput |> part1
-let testResult1 = isSuccess testValue1 expected1
-print "Test 1: %A" testValue1 testResult1
+let isSuccess1 = isSuccess testValue1 expected1
+print "Test: %A" testValue1 isSuccess1
 
 // --------------------------------------------
 // Part 1
 let result1 = loadInput |> part1
-print "Result 1: %A" result1 Info
+print "Result: %A" result1 isSuccess1 
+
+Console.ForegroundColor <- ConsoleColor.Gray
+printfn "------- Part 2 -------"
 
 // --------------------------------------------
 // Test -Part 2
 let testValue2 = loadTestInput |> part2
-let testResult2 = isSuccess testValue2 expected2
-print "Test 2: %A" testValue2 testResult2
+let isSuccess2 = isSuccess testValue2 expected2
+print "Test: %A" testValue2 isSuccess2
 
 // --------------------------------------------
 // Part 2
 let result2 = loadInput |> part2
-print "Result 2: %A" result2 Info
+print "Result: %A" result2 isSuccess2
