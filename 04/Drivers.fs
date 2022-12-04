d3486961dea988dc88a9ae5af7de90be02726b5a
Gustav Gahm <gustav.gahm@sogeti.se>
2022-12-04 19:47:57 +0100

    04

--- 04/Drivers.fs
+++ 04/Drivers.fs
@@ -1,60 +1,46 @@
 module AdventOfCode.Drivers
 
 open AdventOfCode.Utilities
-open FSharpPlus
-open System
 
-let length x = String.length x
-let halfLength x = (length x) / 2
-
-let asSet (s1, s2) = (Set.ofList s1, Set.ofList s2)
-let inBoth (s1, s2) = Set.intersect s1 s2
-
-
-let calculateScore c =
-    match c with
-    | c when Char.IsLower(c) -> int c - 112 + 16
-    | c -> int c - 76 + 38
-
-
-let parseItems rows =
-    rows
-    |> Array.map (fun items -> (items, halfLength items))
-    |> Array.map (fun (items, l) -> (items[.. (l - 1)], items[l..]))
-    |> Array.map (fun (s1, s2) -> (String.toList s1, String.toList s2))
-    |> Array.map asSet
-    |> Array.map inBoth
-    |> Array.map Set.toList
-    |> Array.toList
+let parseRows line =
+    line
+    |> split ","
+    |> Array.map (fun x -> split "-" x)
+    |> Array.map (fun x -> (int x[0], int x[1]))
+    |> Array.map (fun (x, y) -> [ x..y ])
+    |> Array.map (fun x -> set x)
+    |> (fun x -> (x[0], x[1]))
 
 // ----------------------------------------------------------------
 // Part 1
 
-let expected1 = 157
+let isFullOverlap (s1, s2) =
+    Set.isSubset s1 s2 || Set.isSubset s2 s1
+
+let expected1 = 2
 
 let part1 (input: string[]) =
-    let score = input |> parseItems |> flattenList |> List.map calculateScore |> List.sum
+    let rows = input |> Array.map parseRows
 
-    score
+    let overlaps = rows |> Array.map isFullOverlap |> Array.filter id |> Array.length
+
+    overlaps
 
 // ----------------------------------------------------------------
 // Part 2
 
-let expected2 = 70
+let isOverlap (s1, s2) = Set.intersect s1 s2
 
-let parseGroup x =
-    x |> Array.map Set.ofList |> Set.intersectMany |> Set.toArray
-
+let expected2 = 4
 
 let part2 input =
 
-    let groups = input |> Array.map Seq.toList |> Array.splitInto (input.Length / 3)
+    let rows = input |> Array.map parseRows
 
-    let score =
-        groups
-        |> Array.map parseGroup
-        |> flattenArray
-        |> Array.map calculateScore
-        |> Array.sum
+    let overlaps =
+        rows
+        |> Array.map isOverlap
+        |> Array.filter (fun x -> (Set.isEmpty x) = false)
+        |> Array.length
 
-    score
+    overlaps
